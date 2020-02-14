58676a15d880e49580c671d1bb50d2307bd4c3c0
MicahEhrlich <unreal3.ehrlich@gmail.com>
2020-02-14 23:42:14 +0200

    Contacts API
    Contacts API with Get/Post methods

---
+++ server/ContactsAPI/ContactsAPI/Controllers/ContactController.cs
@@ -0,0 +1,74 @@
+﻿using ContactsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Mail;
+
+namespace ContactsAPI.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable dataTable = new DataTable();
+            string query = @"select IdNumber, FullName, Email, BirthDate, Gender, PhoneNum from Contacts";
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactsDB"].ConnectionString))
+            using (var command = new SqlCommand(query, connection))
+            using (var dataAdapter = new SqlDataAdapter(command))
+            {
+                command.CommandType = CommandType.Text;
+                dataAdapter.Fill(dataTable);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, dataTable);
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                long res = 0;
+                if (!long.TryParse(contact.IdNumber,out res)) throw Exception();
+                if (!long.TryParse(contact.PhoneNum, out res)) throw Exception();
+                var mail = new System.Net.Mail.MailAddress(contact.Email);
+                if (mail.Address != contact.Email) throw Exception();
+
+                string query = @"insert into Contacts (IdNumber, FullName, Email, BirthDate, Gender, PhoneNum)
+                values('" + contact.IdNumber+ @"',
+                '" + contact.FullName + @"',
+                '" + contact.Email + @"',
+                '" + contact.BirthDate + @"',
+                '" + contact.Gender + @"',
+                '" + contact.PhoneNum + @"')";
+
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactsDB"].ConnectionString))
+                using (var command = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Added Successfully");
+            } 
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Parameters");
+            }
+        }
+
+        private Exception Exception()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/ContactsAPI/ContactsAPI/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsAPI.Models
+{
+    public class Contact
+    {
+        public string IdNumber { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNum { get; set; }
+
+
+    }
+}
