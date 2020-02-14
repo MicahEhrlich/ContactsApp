f317aed4f861ceda46d38dffb44bf29fd9886d30
MicahEhrlich <unreal3.ehrlich@gmail.com>
2020-02-15 00:41:23 +0200

    Contacts Client React App

--- server/ContactsAPI/ContactsAPI/Controllers/ContactController.cs
+++ server/ContactsAPI/ContactsAPI/Controllers/ContactController.cs
@@ -36,14 +36,10 @@ namespace ContactsAPI.Controllers
             {
                 DataTable dataTable = new DataTable();
 
-                long res = 0;
-                if (!long.TryParse(contact.IdNumber,out res)) throw Exception();
-                if (!long.TryParse(contact.PhoneNum, out res)) throw Exception();
-                var mail = new System.Net.Mail.MailAddress(contact.Email);
-                if (mail.Address != contact.Email) throw Exception();
+                if (!ValidateFields(contact)) throw Exception();
 
                 string query = @"insert into Contacts (IdNumber, FullName, Email, BirthDate, Gender, PhoneNum)
-                values('" + contact.IdNumber+ @"',
+                values('" + contact.IdNumber + @"',
                 '" + contact.FullName + @"',
                 '" + contact.Email + @"',
                 '" + contact.BirthDate + @"',
@@ -59,16 +55,82 @@ namespace ContactsAPI.Controllers
                 }
 
                 return Request.CreateResponse(HttpStatusCode.OK, "Added Successfully");
-            } 
-            catch (Exception ex)
+            }
+            catch (Exception)
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Parameters");
             }
         }
 
+        public HttpResponseMessage Put(Contact contact)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                if (!ValidateFields(contact)) throw Exception();
+
+                string query = @"update Contacts set
+                 FullName = '" + contact.FullName + @"',
+                 Email = '" + contact.Email + @"',
+                 BirthDate = '" + contact.BirthDate + @"',
+                 Gender = '" + contact.Gender + @"',
+                 PhoneNum = '" + contact.PhoneNum + @"'
+                where IdNumber = " + contact.IdNumber + @"";
+
+
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactsDB"].ConnectionString))
+                using (var command = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Updated Successfully");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Parameters");
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+                string query = @"delete from Contacts where IdNumber = " + id;
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactsDB"].ConnectionString))
+                using (var command = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "Deleted Successfully");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Parameters");
+            }
+        }
+
+
         private Exception Exception()
         {
             throw new NotImplementedException();
+        }
+
+        // validate contact fields
+        bool ValidateFields(Contact contact)
+        {
+            long res = 0;
+            if (!long.TryParse(contact.IdNumber, out res)) return false;
+            if (!long.TryParse(contact.PhoneNum, out res)) return false;
+            var mail = new System.Net.Mail.MailAddress(contact.Email);
+            if (mail.Address != contact.Email) return false;
+            return true;
         }
     }
 }
